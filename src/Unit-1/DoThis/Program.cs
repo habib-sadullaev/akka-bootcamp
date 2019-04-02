49f86a2f7badd6f3e7a5a13f44beceef50219e51
habib <h.sadullaev@gmail.com>
2019-04-03 00:26:19 +0500

    Lesson 1.1: Actors and the ActorSystem

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-﻿using Akka.Actor;
+using System.Threading.Tasks;
+using Akka.Actor;
 
 namespace WinTail
 {
@@ -8,24 +9,22 @@ namespace WinTail
     {
         public static ActorSystem MyActorSystem;
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             // initialize MyActorSystem
-            // YOU NEED TO FILL IN HERE
+            MyActorSystem = ActorSystem.Create(nameof(MyActorSystem));
 
             PrintInstructions();
 
             // time to make your first actors!
-            //YOU NEED TO FILL IN HERE
-            // make consoleWriterActor using these props: Props.Create(() => new ConsoleWriterActor())
-            // make consoleReaderActor using these props: Props.Create(() => new ConsoleReaderActor(consoleWriterActor))
-
+            var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()));
+            var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)));
 
             // tell console reader to begin
-            //YOU NEED TO FILL IN HERE
+            consoleReaderActor.Tell("start");
 
             // blocks the main thread from exiting until the actor system is shut down
-            MyActorSystem.WhenTerminated.Wait();
+            await MyActorSystem.WhenTerminated;
         }
 
         private static void PrintInstructions()
